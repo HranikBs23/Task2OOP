76e46de6000241ead75e602f0e4e715849b29964
hranikBs23 <Habibur.Rahman@brainstation-23.com>
2023-04-05 15:57:51 +0600

    task done

--- TASK2_OOP/Program.cs
+++ TASK2_OOP/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Numerics;
+using System.Text;
+using static TASK2_OOP.Person;
 
 namespace TASK2_OOP
 {
@@ -6,7 +9,8 @@ namespace TASK2_OOP
   class Program
     {
 
-        public enum DaysOfWeek
+        //Enum
+     public enum DaysOfWeek
         {
             Monday,
             Tuesday,
@@ -16,32 +20,26 @@ namespace TASK2_OOP
             Saturday,
             Sunday
         }
+
         static void Main(string[] args) {
 
+            Console.WriteLine($"*******************  Brainstation 23 TASK 2     ****************************");
 
-            //Class and Obejct , Method Question 1
+
+            Console.WriteLine("************************** types of properties and methods    ******************************");
+          
             Employee emp1 = new Employee("ANIK","RAHMAN",25,88015211,"BS1364",10000);
             Employee emp2 = new Employee("Tonmoy","Saha",24,8801933,"BS1359",10000);
-
-
 
             emp1.Sayhello();
             emp2.Sayhello();
 
-
-            //set private properties
-
-            //set  internal property
-
-            //call public 
-
-            //call private 
-
-            //call internal method 
-
-            //call public method that uses private
             emp1.Bonus(95);
 
+
+
+            Console.WriteLine("************************** Inheritance   ******************************");
+            
             // Create instances of each class
             Vehicle v1 = new Vehicle("Toyota", "Camry", 2022);
             Car c1 = new Car("Ford", "Mustang", 2022, "OilCooled");
@@ -68,34 +66,71 @@ namespace TASK2_OOP
             Console.WriteLine($"Sedan: {sedan.Make} {sedan.Model} {sedan.Year} {sedan.Engine} {sedan.IsLuxury}");
             sedan.Start(); // Calls Sedan's overridden Start() method
 
-            //abstract
-            Rectangle rect = new Rectangle(5, 10);
+            
 
-            Console.WriteLine("Rectangle area: " + rect.Area());
 
+            ///// *************** Structure *****************************///
 
             // Create an instance of the Person struct
             Person person1 = new Person();
-            person1.Name = "John";
-            person1.Age = 30;
+            person1.Name = "HABIBUR";
+            person1.Age = 27;
 
             // Call the PrintDetails method to print the person's details
             person1.PrintDetails();
 
 
-            /// Refrence 
+            /// *********************   Refrence   **************************  ///
             /// 
-            int a = 10;
-            int b = a;
-            Console.WriteLine($"a = {a}, b = {b}");  // Output: a = 10, b = 10
+            MyClass obj1 = new MyClass();
+            obj1.Value = 10;
+            MyClass obj2 = obj1; // obj2 now references the same object as obj1
+            obj2.Value = 20; // changes the value of the object referenced by obj1
 
-            RefPrim obj1 = new RefPrim();
-            obj1.x = 10;
-            RefPrim obj2 = obj1;
-            obj2.x = 20;
-            Console.WriteLine($"obj1.x = {obj1.x}, obj2.x = {obj2.x}");  // Output: obj1.x = 20, obj2.x = 20
+            Console.WriteLine(obj1.Value); // outputs 20
 
-            ///// Declare arrays of different types
+            int num1 = 10;
+            int num2 = num1; // a copy of the value is created
+            num2 = 20; // changes the value of num2
+
+            Console.WriteLine(num1); // outputs 10
+
+            /// ********************* Constructor Overloading *************//
+            /// 
+            Cricketer cric1 = new Cricketer();
+            Console.WriteLine("Name: " + cric1.Name + ", Age: " + cric1.Age + ", Team: " + cric1.Team);
+
+            Cricketer cric2 = new Cricketer("ANIK");
+            Console.WriteLine("Name: " + cric2.Name + ", Age: " + cric2.Age + ", Team: " + cric2.Team);
+
+            Cricketer cric3 = new Cricketer("RONALDO", 25);
+            Console.WriteLine("Name: " + cric3.Name + ", Age: " + cric3.Age + ", Team: " + cric3.Team);
+
+            Cricketer cric4 = new Cricketer("MESSI", 30, "ARGENTINA");
+            Console.WriteLine("Name: " + cric4.Name + ", Age: " + cric4.Age + ", Team: " + cric4.Team);
+
+
+
+
+            ////*********** Method overloading ********* //////////////
+            ///
+
+
+            Calculator calc = new Calculator();
+
+            int sum1 = calc.Add(1, 2);
+            Console.WriteLine("Sum of 1 and 2 is: " + sum1);
+
+            int sum2 = calc.Add(1, 2, 3);
+            Console.WriteLine("Sum of 1, 2, and 3 is: " + sum2);
+
+            double sum3 = calc.Add(1.5, 2.5);
+            Console.WriteLine("Sum of 1.5 and 2.5 is: " + sum3);
+            
+            
+            ////////***********      Arrays **********//////////
+
+            ///// Declaring arrays of different types
             bool[] boolArray = new bool[15];
             char[] charArray = new char[15];
             int[] intArray = new int[15];
@@ -103,7 +138,7 @@ namespace TASK2_OOP
             double[] doubleArray = new double[15];
             string[] stringArray = new string[15];
 
-            // Insert dummy values to arrays
+            ///// Inserting dummy values to arrays
             for (int i = 0; i < 15; i++)
             {
                 boolArray[i] = (i % 2 == 0); // alternate true and false
@@ -113,8 +148,6 @@ namespace TASK2_OOP
                 doubleArray[i] = i * 1.5;
                 stringArray[i] = "Element " + i;
             }
-
-            //For loop
             // Using for loop
             for (int i = 0; i < 15; i++)
             {
@@ -129,51 +162,216 @@ namespace TASK2_OOP
                 Console.WriteLine($"doubleArray[{i}] = {doubleArray[i]}");
                 Console.WriteLine($"stringArray[{i}] = {stringArray[i]}");
             }
-
             // Using foreach loop
-            foreach (bool b in boolArray)
+            foreach (bool x in boolArray)
             {
-                if (Array.IndexOf(boolArray, b) == 5) continue; // Skip index 5
+                if (Array.IndexOf(boolArray, x) == 5) continue; // Skip index 5
 
-                if (Array.IndexOf(boolArray, b) > 10) break; // Stop at index 10
+                if (Array.IndexOf(boolArray, x) > 10) break; // Stop at index 10
 
-                Console.WriteLine($"boolArray[{Array.IndexOf(boolArray, b)}] = {b}");
+                Console.WriteLine($"boolArray[{Array.IndexOf(boolArray, x)}] = {x}");
             }
 
-            foreach (char c in charArray)
-            {
-                if (Array.IndexOf(charArray, c) == 5) continue; // Skip index 5
+            //***********               ENUM                  ***********//
 
-                if (Array.IndexOf(charArray, c) > 10) break; // Stop at index 10
+            // Convert string to enum
+            string inputString = "Friday";
+            DaysOfWeek DayFromString = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), inputString);
+            Console.WriteLine($"Enum value from string: {DayFromString}");
 
-                Console.WriteLine($"charArray[{Array.IndexOf(charArray, c)}] = {c}");
-            }
+            // Convert int to enum
+            int inputInt = 3;
+            DaysOfWeek DayFromInt = (DaysOfWeek)inputInt;
+            Console.WriteLine($"Enum value from int: {DayFromInt}");
 
-            ///ENum
+            // Convert enum to string
+            DaysOfWeek DayOfWeek = DaysOfWeek.Wednesday;
+            string DayOfWeekString = DayOfWeek.ToString();
+            Console.WriteLine($"String value from enum: {DayOfWeekString}");
+
+            // Convert enum to int
+            int DayOfWeekInt = (int)DayOfWeek;
+            Console.WriteLine($"Int value from enum: {DayOfWeekInt}");
+
+
+            /***********       List       ****************///
+            List<int> IntList = new List<int> { 1, 2, 3, 4, 5 };
+            List<string> StringList = new List<string> { "Ronaldo", "Messi", "Neymar", "Benzama", "Ramos" };
+            List<double> DoubleList = new List<double> { 1.23, 4.56, 7.89, 2.34, 5.67 };
+
+            // Find out the number of elements in each list
+            Console.WriteLine($"IntList Count: {IntList.Count}");
+            Console.WriteLine($"StringList Count: {StringList.Count}");
+            Console.WriteLine($"DoubleList Count: {DoubleList.Count}");
+
+            // Add elements to a list
+            IntList.Add(6);
+            StringList.Add("Mbappe");
+            DoubleList.Add(8.9);
+
+            // Remove elements from a list
+            IntList.Remove(3);
+            StringList.Remove("Ramos");
+            DoubleList.RemoveAt(2);
+
+            // Sort a list
+            IntList.Sort();
+            StringList.Sort();
+            DoubleList.Sort();
+
+            // Reverse a list
+            IntList.Reverse();
+            StringList.Reverse();
+            DoubleList.Reverse();
+
+            // Insert an element at a specific index
+            IntList.Insert(2, 7);
+            StringList.Insert(4, "grape");
+            DoubleList.Insert(1, 6.78);
+
+            // Find the index of an element
+            int index1 = IntList.IndexOf(5);
+            int index2 = StringList.IndexOf("date");
+            int index3 = DoubleList.IndexOf(4.56);
+            Console.WriteLine($"Index of 5 in IntList: {index1}");
+            Console.WriteLine($"Index of \"date\" in StringList: {index2}");
+            Console.WriteLine($"Index of 4.56 in DoubleList: {index3}");
+
+            // Check if an element is in a list
+            bool contains1 = IntList.Contains(6);
+            bool contains2 = StringList.Contains("Mbappe");
+            bool contains3 = DoubleList.Contains(8.9);
+            Console.WriteLine($"Does IntList contain 3?: {contains1}");
+            Console.WriteLine($"Does StringList contain \"Mbappe\"?: {contains2}");
+            Console.WriteLine($"Does DoubleList contain 8.9?: {contains3}");
+
+            // Convert a list to an array
+            int[] IntArray = IntList.ToArray();
+            string[] StringArray = StringList.ToArray();
+            double[] DoubleArray = DoubleList.ToArray();
+
+
+            // *********** DATE TIME ********************//
+            //for bd time culture info
+            // Creating a DateTime object representing the current date and time
+            DateTime now = DateTime.Now;
+
+            // Print the date and time 
+            Console.WriteLine("Current date and time: " + now);
+            Console.WriteLine("Short date: " + now.ToShortDateString());
+            Console.WriteLine("Long date: " + now.ToLongDateString());
+            Console.WriteLine("Short time: " + now.ToShortTimeString());
+            Console.WriteLine("Long time: " + now.ToLongTimeString());
+
+            // Create a TimeSpan object 
+            TimeSpan duration = new TimeSpan(3, 30, 0);
+
+            // Add the duration to the current date and time to get a new DateTime object
+            DateTime future = now.Add(duration);
+
+            // Print the new date and time
+            Console.WriteLine("Future date and time: " + future);
+
+            // Subtract the current date and time from the future date and time to get a TimeSpan object
+            TimeSpan difference = future - now;
+
+            // Print the difference in hours, minutes, and seconds
+            Console.WriteLine("Time difference: " + difference.Hours + " hours, " + difference.Minutes + " minutes, " + difference.Seconds + " seconds");
+
+
+            //********** String **************//
+
+            string text = "  Brainstation 23   ";
+
+            // Trim whitespace
+            string TrimmedText = text.Trim();
+            Console.WriteLine(TrimmedText);
+
+            // To upper case
+            string UpperText = text.ToUpper();
+            Console.WriteLine(UpperText);
+
+            // To lower case
+            string LowerText = text.ToLower();
+            Console.WriteLine(LowerText);
+
+            // Replace
+            string ReplacedText = text.Replace("23", "3000");
+            Console.WriteLine(ReplacedText);
+
+            // Split
+            string[] Words = text.Split(' ');
+            Console.WriteLine(Words[0]);
+            Console.WriteLine(Words[1]);
+            Console.WriteLine(Words[2]);
+            Console.WriteLine(Words[3]);
+
+            // Substring
+            string Substring = text.Substring(2, 5);
+            Console.WriteLine(Substring);
+
+            // Length
+            int length = text.Length;
+            Console.WriteLine(length); // Output: 16
+
+            // Concatenate
+            string _a = "BS";
+            string _b = "13";
+            string _c = "64";
+            string ConcatenatedText = string.Concat(_a, " ", _b, _c);
+            Console.WriteLine(ConcatenatedText);
+
+            // Format
+            int Goals = 832;
+            string Player = "Ronaldo";
+            string FormattedText = string.Format("Goat! He scored {0} and Remember the name {1}.", Goals, Player);
+            Console.WriteLine(FormattedText);
+
+            ///// ******************* StringBuilder ***********************/////
+
+            StringBuilder sb = new StringBuilder("Brain Station");
+
+            // Append a string to the StringBuilder
+            sb.Append(" 232"); 
+
+            // Insert a string at a specific position
+            sb.Insert(5, ", ");
+
+            // Remove characters from the StringBuilder
+            sb.Remove(18, 1);
+
+            // Replace a character in the StringBuilder
+            sb.Replace("a", "A");
+
+            // Convert the StringBuilder to a string
+            string str = sb.ToString();
+
+            Console.WriteLine(str);
+
+
+
+            ////// ************************  Abstract ***********************///
             ///
-        
 
-        // Convert string to enum
-        string inputString = "Friday";
-        DaysOfWeek dayFromString = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), inputString);
-        Console.WriteLine($"Enum value from string: {dayFromString}");
+            BS23 e1 = new Mohakhali();
+            e1.Office();
 
-        // Convert int to enum
-        int inputInt = 3;
-        DaysOfWeek dayFromInt = (DaysOfWeek)inputInt;
-        Console.WriteLine($"Enum value from int: {dayFromInt}");
+            BS23 e2 = new Mirpur();
+            e2.Office();
 
-         // Convert enum to string
-         DaysOfWeek dayOfWeek = DaysOfWeek.Wednesday;
-        string dayOfWeekString = dayOfWeek.ToString();
-        Console.WriteLine($"String value from enum: {dayOfWeekString}");
 
-          // Convert enum to int
-         int dayOfWeekInt = (int)dayOfWeek;
-        Console.WriteLine($"Int value from enum: {dayOfWeekInt}");
+
+            /// **************** InterFace **********************////
+            /// MusicPlayer player = new MusicPlayer();
+            MusicPlayer player = new MusicPlayer();
+            player.Play();
+            player.Pause();
+            player.Stop();
+
+
 
         }
-}
+    }
     
 
     }
--- TASK2_OOP/oop1.cs
+++ TASK2_OOP/oop1.cs
@@ -7,7 +7,15 @@ using System.Threading.Tasks;
 namespace TASK2_OOP
 {
 
-    
+ 
+    /// <summary>
+    ///  **************   ALL THE CLASSES **********************////
+    /// </summary>
+    /// 
+
+
+
+    ///  ***********  types of properties and methods ****************** //
     class Employee
     {
         //public properties
@@ -20,62 +28,64 @@ namespace TASK2_OOP
         private int Number { get; set; }
         //protected properties
 
-        protected  decimal Salary { get; set; }
+        protected decimal Salary { get; set; }
 
         //Internal property
         internal string EmpId { get; set; }
 
         //Public Method
-        public void Sayhello () {
+        public void Sayhello()
+        {
 
             Console.WriteLine($"Hello, My name is {FirstName} {LastName}, Age: {Age} and My ID is: {EmpId}. My contact Number is {Number}. Salary:{Salary}");
 
         }
 
-      
+
 
 
         //Protected method 
-        protected void Raise (decimal amount)
+        protected void Raise(decimal amount)
         {
             Salary += amount;
             Console.WriteLine($"Congrats! Salary increased and new salary is {Salary}");
         }
 
         //Internal method
-        internal void PayRoll ()
+        internal void PayRoll()
         {
             Console.WriteLine($" processing payroll for employee {FirstName}  {LastName} {EmpId}");
         }
-        //private Method
-        private decimal  CalcBonus (decimal performance)
+
+        //Private Method
+        private decimal CalcBonus(decimal performance)
         {
-            decimal bonus =0;
+            decimal bonus = 0;
             if (performance >= 90)
             {
                 bonus = Salary * 0.2m;
             }
-            else if(performance >= 80)
+            else if (performance >= 80)
             {
                 bonus = Salary * 0.1m;
             }
             return bonus;
         }
 
-        //Public method that uses the private method 
+        //Public method that uses the Private method 
 
         public void Bonus(decimal performance)
         {
-            decimal bonus = CalcBonus (performance);
+            decimal bonus = CalcBonus(performance);
             Salary += bonus;
             Console.WriteLine($"Congrats {FirstName}! You Got Bonus {bonus}");
         }
 
         // constructor
 
-        //
 
-       
+
+
         public Employee(string FirstName, string LastName, int Age, int Number, string EmpId, decimal Salary)
         {
             this.FirstName = FirstName;
@@ -85,18 +95,13 @@ namespace TASK2_OOP
             this.Salary = Salary;
             this.EmpId = EmpId;
         }
-        public Employee(string Firstname, string Lastname)
 
-        {
-            this.FirstName = Firstname;
-            this.LastName = Lastname;
-
-        }
 
 
     }
 
-    // Declare a struct named Person
+    
+    // *************   Declare a struct named Person *********************//
     public struct Person
     {
         // Properties
@@ -109,7 +114,244 @@ namespace TASK2_OOP
             Console.WriteLine("Name: " + Name);
             Console.WriteLine("Age: " + Age);
         }
+
+        //// Constructor Overloading 
+
+
+        public class Cricketer
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Team { get; set; }
+
+            //No parameter
+            public Cricketer()
+            {
+                Name = "Unknown";
+                Age = 0;
+                Team = "Unknown";
+            }
+
+            //single parameter
+            public Cricketer(string name)
+            {
+                Name = name;
+                Age = 0;
+                Team = "Unknown";
+            }
+
+            //two parameter
+            public Cricketer(string name, int age)
+            {
+                Name = name;
+                Age = age;
+                Team = "Unknown";
+            }
+
+            //all
+            public Cricketer(string name, int age, string team)
+            {
+                Name = name;
+                Age = age;
+                Team = team;
+            }
+        }
+
+
+
+        //********** CLasses are reference types ******** ////
+        public class MyClass
+        {
+            public int Value;
+        }
+
+        ///// Method Overloading 
+        ///
+
+
+        public class Calculator
+            {
+                public int Add(int a, int b)
+                {
+                    return a + b;
+                }
+
+                public int Add(int a, int b, int c)
+                {
+                    return a + b + c;
+                }
+
+                public double Add(double a, double b)
+                {
+                    return a + b;
+                }
+            }
+
+
+        }
+
+
+
+    // ********************* Inheritance ******************************//
+
+    // Base class
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        // Constructor
+        public Vehicle(string Make, string Model, int Year)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Year = Year;
+        }
+
+        public virtual void Start()
+        {
+            Console.WriteLine("Vehicle starting");
+        }
+    }
+
+    // Derived class using single inheritance
+    public class Car : Vehicle
+    {
+        public string Engine { get; set; }
+
+        // Constructor Inheritance
+        public Car(string Make, string Model, int Year, string Engine) : base(Make, Model, Year)
+        {
+            this.Engine = Engine;
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Car starting");
+        }
+    }
+
+    // Derived class using multilevel inheritance
+    public class Sedan : Car
+    {
+        public bool IsLuxury { get; set; }
+
+        // Constructor
+        public Sedan(string Make, string Model, int Year, string Engine, bool IsLuxury) : base(Make, Model, Year, Engine)
+        {
+            this.IsLuxury = IsLuxury;
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Sedan starting");
+        }
+    }
+
+    // Derived class using hierarchical inheritance
+    public class Truck : Vehicle
+    {
+        public string Color { get; set; }
+
+        // Constructor
+        public Truck(string Make, string Model, int Year, string Color) : base(Make, Model, Year)
+        {
+            this.Color = Color;
+        }
+
+        public override void Start()
+        {
+            Console.WriteLine("Truck starting");
+        }
     }
 
 
+    ///// ***************** Multiple Inheritance ********************* ////
+    ///
+    // First interface
+    public interface IAcademicInfo
+    {
+        int? StudentId { get; set; }
+        string? Department { get; set; }
+        double? GradePointAverage { get; set; }
+    }
+
+    // Second interface
+    public interface IPersonalInfo
+    {
+        string? Name { get; set; }
+        string? Address { get; set; }
+        string? PhoneNumber { get; set; }
+    }
+
+    // Class implementing multiple interfaces
+    public class Student : IAcademicInfo, IPersonalInfo
+    {
+        public int? StudentId { get; set; }
+        public string? Department { get; set; }
+        public double? GradePointAverage { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+
+
+
+    ////// ****************************** Abstract ****************************/////
+    ///
+
+    public abstract class BS23
+    {
+        public abstract void Office();
+    }
+
+    public class Mohakhali : BS23
+    {
+        public override void Office()
+        {
+            Console.WriteLine("You are in Mohakhali...");
+        }
+    }
+
+    public class Mirpur : BS23
+    {
+        public override void Office()
+        {
+            Console.WriteLine("You are in Mirpur...");
+        }
+    }
+    //**************************    Interface ************************* ///////
+
+    public interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+
+    public class MusicPlayer : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music...");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Pausing music...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping music...");
+        }
+    }
+
 }
+
+
+
+
+
